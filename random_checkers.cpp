d4b2846c147a6fd5054c72b7615fba2114258562
Laurin <lafisher@alaska.edu>
2018-01-17 16:23:31 -0900

    Added basic files.

---
+++ random_checkers.cpp
@@ -0,0 +1,12 @@
+
+#ifndef RANDOM_CHECKERS_INCLUDED
+#define RANDOM_CHECKERS_INCLUDED
+
+class randomCheckers{
+public: 
+	randomCheckers(); 
+	void play(); 
+private: 
+}; 
+
+#endif /* RANDOM_CHECKERS_INCLUDED */ 
