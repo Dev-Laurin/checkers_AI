24d1948ced35d5ce849ddbed6312139a622658b4
Jason Bright <jtbright@alaska.edu>
2018-01-18 15:20:12 -0900

    updated stdBoard reference

--- random_checkers.h
+++ random_checkers.h
@@ -1,25 +1,26 @@
 #ifndef RANDOM_CHECKERS_INCLUDED
 #define RANDOM_CHECKERS_INCLUDED
 
-#include <vector>
-using std::vector; 
+#include <stdint.h>
+#include <string>
+using std::string;
 
-class pon{
-public: 
-	int x; //positions on board
-	int y; 
-}; 
+typedef int32_t i32;
 
-class randomCheckers{
-public: 
-	randomCheckers(); 
-	void play(); 
-	/*firstPlayersMove*/ void secondPlayersMove(); 
-private: 
-	int bk; 
-	int red; 
-	int bkKing; 
-	int redKing; 
-}; 
+class stdBoard {
+public:
+	i32 black;  //black pieces - pawns and kings
+	i32 red;  //red pieces - pawns and kings
+	i32 blackK; //black kings
+	i32 redK;  //red kings
+	void stdBoard();  //4,095; 4,293,918,720
+	void stdBoard(string board);
+	void stdBoard(i32 b, i32 r, i32 bK, i32 rK);
+	string strBoard();  //returns string representation of a board.
+	//generateMoves - fills an array/vector with the valid moves possible on the board.
+	//Returns false if no valid moves found.
+	bool generateMoves(stdBoard boardList[]);  //boardlist is previously allocated vector/array for boards to be stored in.  Return is false if no moves
+	strdBoard flipBoard();  //returns view of board from other side.
+};
 
 #endif /* RANDOM_CHECKERS_INCLUDED */ 
