2f6b917b897b2d8c28cc086c24834c22e7ad41cd
Laurin <lafisher@alaska.edu>
2018-01-18 18:13:01 -0900

    Have SFML up and running.

--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
 all: main.cpp
-	g++ -std=c++14 main.cpp random_checkers.cpp 
+	g++ -std=c++14 main.cpp random_checkers.cpp -lsfml-graphics -lsfml-window -lsfml-system
+	./a.out 
--- main.cpp
+++ main.cpp
@@ -2,10 +2,26 @@
 using std::cout; 
 using std::endl; 
 #include "random_checkers.h"
+#include <SFML/Graphics.hpp>
 
 int main(){
 
-	stdBoard b; 
-	b.draw(); 
+	sf::RenderWindow window(sf::VideoMode(200,200), "SFML works!");
+	sf::CircleShape shape(100.f); 
+	shape.setFillColor(sf::Color::Green); 
+
+	while(window.isOpen())
+	{
+		sf::Event event; 
+		while (window.pollEvent(event)){
+			if(event.type == sf::Event::Closed)
+				window.close(); 
+		}
+
+		window.clear(); 
+		window.draw(shape); 
+		window.display(); 
+	}
+	
 	return 0; 
 }
