7255d1acc0d38c8b23bc7b4d87c8b50d11ad5f44
Jason Bright <jtbright@alaska.edu>
2018-01-30 17:05:02 -0900

    added old checker move generator

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all: main.cpp
 	g++ -std=c++14 main.cpp -lsfml-graphics -lsfml-window -lsfml-system
 	./a.out 
-test: testBoard.cpp
+test: testBoard.cpp old_movegenerator.cpp
 	g++ -std=c++14 testBoard.cpp
 	./a.out
---
+++ old_movegenerator.h
@@ -0,0 +1,33 @@
+#ifndef MOVEGENERATOR
+#define MOVEGENERATOR
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+namespace altCheckers
+{
+	const unsigned int MAXBOARDS = 20;
+	const unsigned int BOARDSIZE = 32;
+	const unsigned int VALIDMOVEWIDTH = 8;
+	const unsigned char REDPAWN = 'r';
+	const unsigned char BLACKPAWN = 'b';
+	const unsigned char REDKING = 'R';
+	const unsigned char BLACKKING = 'B';
+	const unsigned char EMPTYCELL = ' ';
+
+	void PrintBoard(char Board[32]);
+	void PrintBoardLine(char Board[32]);
+
+	void kingMaker(char Boardret[]);
+
+	bool jumpcheck(const char ValidMoves[], const char BoardStr[], int x, int pieceValue, char Boardret[MAXBOARDS][BOARDSIZE], int & numMoves);
+
+	int MoveGenerator(char BoardStr[], char Boardret[MAXBOARDS][BOARDSIZE], bool isBlack = true);
+
+	void flipBoard(char BoardStr[]);
+	void flipBoard(std::string & Board);
+}
+#endif
