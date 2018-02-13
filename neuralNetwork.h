10c83db578162fd4b90e613c2cbaabbcce72db3b
Dev-Laurin <lafisher@alaska.edu>
2018-02-12 16:42:08 -0900

    Have neural network evaluating a board based on random weights.

--- neuralNetwork.h
+++ neuralNetwork.h
@@ -7,11 +7,18 @@
 #include <vector>
 using std::cout;
 using std::endl;
+#include "board.h"
 
 class NN{
 public:
 	//Set weights to random values
 	NN(std::vector<int>& nodeSizes){
+		//Save the node configuration
+		nodes.resize(nodeSizes.size());
+		for(int i=0; i<nodeSizes.size(); ++i){
+			nodes[i].resize(nodeSizes[i], 0); 
+		}
+		//Random number generator
 		gen.seed(time(0)); 
     	std::uniform_real_distribution<double> dis(-1.0,1.0);
 
@@ -25,8 +32,78 @@ public:
 			}
 		}
 	}
+	//Given a board, calculate the output of the NN 
+	int calculateBoard(stdBoard & board){
+		//Get board input and place into node inputs
+		getBoardInput(board); 
+
+		//iterate through network 
+		for(int net=0; net<nodes.size()-1; ++net){
+			int netIndex = 0;
+			if(net!=0)
+				netIndex = net - 1;
+			int networkIndex = 0; 
+			//for each node, sum up the weights & firing value
+			for(int node=0; node<nodes[net+1].size(); ++node){
+				double sumWeights = 0;
+				for(int weight=0; weight<nodes[net].size(); ++weight){
+					sumWeights+= network[netIndex][networkIndex] * nodes[net][weight];
+					++networkIndex; 
+				}		
+
+				//Apply the sigmoid function to the weights & fires
+				double sigmoidOutput = sigmoid(sumWeights);
+				if(sigmoidOutput>0.55){
+					//it fires, output a 1 ?
+					nodes[net+1][node] = 1; 
+				} 
+				else if(sigmoidOutput<0.45){
+					nodes[net+1][node] = -1; 
+				}
+				else{
+					//it doesn't fire, output 0 
+					nodes[net+1][node] = 0; 
+				}
+		
+			}  
+		}
+
+		//the output
+		return nodes[nodes.size()-1][0]; 
+	}
+	//Puts board inputs into nodes for NN calculating
+	void getBoardInput(stdBoard & board){
+		for(int i=0; i<board.str().size(); ++i){
+			//for each position on board
+			char pos = board.str()[i]; 
+
+			if(pos=='b'){
+				nodes[0][i] = 1; 
+			}
+			else if(pos=='r'){
+				nodes[0][i] = -1; 
+			}
+			else if(pos==' '){
+				//is a blank space
+				nodes[0][i] = 0; 
+			}			
+			else if(pos=='B'){
+				nodes[0][i] = 1.4; 
+			}
+			else{ //Red king 'R'
+				nodes[0][i] = -1.4; 
+			}
+
+		}
+	}
+	//Given a number, calculate a sigmoid function output 
+	double sigmoid(double num){
+		double e = 2.71828182845; 
+		return 1/(1 + pow(e, -num)); 
+	}
 	std::vector<std::vector<double > > network;
     std::mt19937 gen; //(time(0));
+    std::vector<std::vector<double >> nodes; 
 };
 
 #endif /* NEURAL_NETWORK_H */
--- testNN.cpp
+++ testNN.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 using std::cout; 
 using std::endl; 
+#include "board.h"
 
 int main(){
 
@@ -16,11 +17,15 @@ int main(){
 		cout << "Network layers:  " << n.network.size() << endl; 
 		for(int i=0; i<n.network.size(); ++i){
 			std::cout << "Dendrite sizes: " << n.network[i].size() << std::endl; 
+			
+			// for(int j=0; j<n.network[i].size(); ++j){
+			// 	cout << n.network[i][j] << endl; 
+			// }
 		}
 	}
 
 	//Test a bigger network
-	std::vector<int> nodes{10, 50, 50, 50, 100, 50, 1}; 
+	std::vector<int> nodes{32, 1}; //50, 50, 50, 100, 50, 1}; 
 	NN bigN(nodes); 
 
 	cout << "Bigger Neural Network > 10,000 weights." << endl;
@@ -29,7 +34,53 @@ int main(){
 	for(int i=0; i<bigN.network.size(); ++i){
 		weights += bigN.network[i].size(); 
 		cout << "Dendrite sizes: " << bigN.network[i].size() << endl;
+		// for(int j=0; j<bigN.network[i].size(); ++j){
+		// 	cout << bigN.network[i][j] << endl; 
+		// }
 	}
 	cout << "Counted network weights: " << weights << endl; 
+
+	cout << "Testing Board input-output" << endl; 
+	cout << "Nodes "<< endl; 
+	cout << "Nodes size [i]: "<< bigN.nodes.size() << endl; 
+	for(int i=0; i<bigN.nodes.size(); ++i){
+		for(int j=0; j<bigN.nodes[i].size(); ++j){
+			cout << bigN.nodes[i][j] << " ";  
+		}
+		cout << endl; 
+	}
+	stdBoard b; 
+	//b.updateBoard("           r       b            "); 
+	bigN.getBoardInput(b); 
+	cout << "Nodes size [i]: "<< bigN.nodes.size() << endl; 
+	for(int i=0; i<bigN.nodes.size(); ++i){
+		for(int j=0; j<bigN.nodes[i].size(); ++j){
+			cout << bigN.nodes[i][j] << " ";  
+		}
+		cout << endl; 
+	}	
+	cout << "First board evaluation: " << bigN.calculateBoard(b);
+	cout << endl; 
+
+	cout << "Node vector after eval: " << endl; 
+		cout << "Nodes size [i]: "<< bigN.nodes.size() << endl; 
+	for(int i=0; i<bigN.nodes.size(); ++i){
+		for(int j=0; j<bigN.nodes[i].size(); ++j){
+			cout << bigN.nodes[i][j] << " ";  
+		}
+		cout << endl; 
+	}	
+
+	cout << "Weights: " << endl; 
+	double sum = 0; 
+	for(int i=0; i<bigN.network.size(); ++i){
+		for(int j=0; j<bigN.network[i].size(); ++j){
+			cout << bigN.network[i][j] << " "; 
+			sum += (bigN.network[i][j] * bigN.nodes[i][j]); 
+		}
+		cout << endl; 
+	}
+	cout << "Sum of weights: " << sum << endl; 
+	cout << "Sigmoid of : " << bigN.sigmoid(sum) << endl; 
 
 }
