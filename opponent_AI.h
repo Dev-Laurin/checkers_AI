74737df87c519271bf128dbff93883b51898ff45
Dev-Laurin <lafisher@alaska.edu>
2018-03-07 15:40:36 -0900

    Fixed bug.

--- opponent_AI.h
+++ opponent_AI.h
@@ -58,6 +58,53 @@ public:
 		}
 	}
 
+	stdBoard getBoardMove(stdBoard&b){
+	switch(AIType_){
+		//Random Move Picker AI
+		case 'r': {
+			//get random number
+			stdBoard possibleBoards[30];
+			int moves = b.genMoves(possibleBoards,1); //red
+			int randMove;
+
+			if(moves==1){
+				randMove = 0;
+			}
+			else{
+				std::mt19937 gen(time(0)); //seed
+				std::uniform_int_distribution<int> dis(0,moves-1);
+				randMove = dis(gen);
+			}
+			return possibleBoards[randMove];
+			break;
+		}
+		case 'g': {
+		    stdBoard possibleBoards[32];
+		    stdBoard tBoard = b.flip();
+		    int moves = tBoard.genMoves(possibleBoards,0);
+		    int selectMove = 0;
+		    sNN moveVal = beta(possibleBoards[0],7);
+		    sNN tempMove;
+		    for (int i = 1;i<moves;++i) {
+                tempMove = beta(possibleBoards[i],7);
+                //cout << "moveVal: " << moveVal << " tempMove: " << tempMove << endl;
+                //cout << "Highest: " << HIGHEST << " Lowest: " << LOWEST << endl;
+                if (tempMove > moveVal) {
+                    //better move found
+                    moveVal = tempMove;
+                    selectMove = i;
+                }
+		    }
+		    tBoard = possibleBoards[selectMove].flip();
+		    return tBoard;
+		}
+		default:
+			cout << "That AI Doesn't exist" << endl;
+			stdBoard error; 
+			return error;
+		}
+	}
+
 	char AIType_;
 };
 
--- playgame.cpp
+++ playgame.cpp
@@ -1,1 +1,38 @@
+//playgame.cpp 
+//3-6-18 Revised 
+//Play a game of NN's
 
+#include <iostream>
+#include "neuralNetwork.h"
+#include <vector> 
+using std::vector; 
+#include "board.h"
+#include "opponent_AI.h"
+
+int main(){
+	vector<int> nodes{32, 40, 10, 1};
+	NN blondie24(nodes, "blondie24");
+
+	vector<int> nodes2{32, 91, 50, 1}; 
+	NN other(nodes2, "other"); 
+
+	//start a new game 
+	stdBoard b; 
+
+	//First 3 ply are random
+	Opponent random('r'); 
+
+	//200 moves max 
+	for(int i=0; i<200; ++i){
+
+		//other start first 
+		if(i<3){
+			//random moves first 
+			b = random.getBoardMove(b); 
+		}
+
+		
+
+	}
+	return 0; 
+}
