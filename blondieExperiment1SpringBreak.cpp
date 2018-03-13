9a4f6eb43602311ac6ab003cb6934f1b5729ffa8
Jason Bright <jtbright@alaska.edu>
2018-03-13 05:33:14 -0800

    updated tournament code to reduce killed to 5 and prevent NNs from playing themselves

--- blondieExperiment1SpringBreak.cpp
+++ blondieExperiment1SpringBreak.cpp
@@ -1,133 +1,137 @@
 //blondieExperiment1SpringBreak
 //To run the first blondie24 experiment over spring break
-	//to get errors early rather than later with NN. 
+	//to get errors early rather than later with NN.
 //3-12-18
 
 #include <iostream>
-using std::cout; 
-using std::endl; 
+using std::cout;
+using std::endl;
 #include <vector>
-using std::vector; 
+using std::vector;
 #include "neuralNetwork.h"
-#include <boost/filesystem.hpp> 
+#include <boost/filesystem.hpp>
 #include "playgame.h"
-#include <algorithm> 
+#include <algorithm>
 
 int main(){
-	vector<int> nodes{32, 40, 10, 1}; 
-	NN blondie24(nodes, "blondie");  
+	vector<int> nodes{32, 40, 10, 1};
+	NN blondie24(nodes, "blondie");
 
 	//file variables
 	string parentDirectory = "NeuralNetworkFiles/";
-	boost::filesystem::path current_path(boost::filesystem::current_path()); 
+	boost::filesystem::path current_path(boost::filesystem::current_path());
 
 
-	//Population = 30 
-	int population = 30; 
+	//Population = 30
+	int population = 30;
 	vector<NN> NeuralNets(population, blondie24);
 	for(int i=0; i<population; ++i){
 		NeuralNets[i] = NN(nodes, "blondie");
 
-		//First generation 
-		string initialPath = parentDirectory + NeuralNets[i].familyName + 
-			"/" + "GEN" + to_string(NeuralNets[i].generation) + 
-			"/"; 
-		boost::filesystem::path path = current_path; 
-		string slash  = "/" + initialPath; 
-		path += slash.c_str(); 
+		//First generation
+		string initialPath = parentDirectory + NeuralNets[i].familyName +
+			"/" + "GEN" + to_string(NeuralNets[i].generation) +
+			"/";
+		boost::filesystem::path path = current_path;
+		string slash  = "/" + initialPath;
+		path += slash.c_str();
 		boost::filesystem::path dir(path);
 		boost::filesystem::create_directories(dir);
 		if(boost::filesystem::is_directory(dir)){
 			NeuralNets[i].saveToFile(initialPath + "NN" + to_string(i));
 		}
 		else{
-			cout << "Error, directory not created in "; 
+			cout << "Error, directory not created in ";
 			cout << "before-tournament." << endl;
 			cout << path << endl;
 		}
-		 
+
 	}
 
-	int tournamentNum = 0;  
+	int tournamentNum = 0;
 	while(true){
-		
-		//For choosing random opponent within NNs 
+
+		//For choosing random opponent within NNs
 		std::mt19937_64 gen(time(0));
 		std::uniform_int_distribution<int> dis(0,NeuralNets.size());
 
-		//150 game tournament = 30 NNs * 5 games each 
+		//150 game tournament = 30 NNs * 5 games each
 		++tournamentNum;
-		int gamesPerNetwork = 5; 
-		int gameNum = 0; 
+		int gamesPerNetwork = 5;
+		int gameNum = 0;
 		for(int i=0; i<NeuralNets.size(); ++i){
-			//Each NN playes 5 games as Red selecting 
+			//Each NN playes 5 games as Red selecting
 				//random NN opponent
 
 			for(int games=0; games<gamesPerNetwork; ++games){
 				//Choose random opponent
-				int randIndex = dis(gen); 
-				//Play a game 
+				int randIndex;
+				do { //Prevent the AI from playing itself.
+          randIndex = dis(gen);
+				} while (randIndex == i);
+				//Play a game
 				boost::filesystem::path allPath = current_path;
 				string gameDirectory = parentDirectory + "TOUR" +
 					to_string(tournamentNum) + "/GAME" + to_string(gameNum);
-				string slash = "/" + gameDirectory; 
-				allPath += slash.c_str(); 
-				//Make the path 
+				string slash = "/" + gameDirectory;
+				allPath += slash.c_str();
+				//Make the path
 				boost::filesystem::path dir(allPath);
 				boost::filesystem::create_directories(dir);
 				if(boost::filesystem::is_directory(dir)){
-					++gameNum; 
-					playGame(NeuralNets[i], NeuralNets[randIndex], 
-						gameDirectory); 
+					++gameNum;
+					playGame(NeuralNets[i], NeuralNets[randIndex],
+						gameDirectory);
 				}
 				else{
 					cout << "Game DIR not correct." << endl;
-					cout << gameDirectory << endl; 
+					cout << gameDirectory << endl;
 					cout << allPath << endl;
 				}
 
-				
+
 			}
 
 		}
 
 		//Tournament ended, choose the top 15 Neural Networks that
-			//will generate children 
-		//Sort the vector by wins 
-		std::sort(NeuralNets.begin(), NeuralNets.end(), 
+			//will generate children
+		//Sort the vector by wins
+		std::sort(NeuralNets.begin(), NeuralNets.end(),
 			[](const NN & a, const NN & b) -> bool{
-				return a.wins < b.wins; 
-			}); 
+				return a.wins < b.wins;
+			});
 
 		//Last 15 will generate children (higher wins)
-			//overtop the first 15 
-		for(int i=NeuralNets.size()-1; i>NeuralNets.size()/2; --i){
+			//overtop the first 15
+		//for(int i=NeuralNets.size()-1; i>NeuralNets.size()/2; --i){
+		for(int i=NeuralNets.size()-1; i>5; --i){  //Only kill and replace 5.
 			//save parent to file
-			boost::filesystem::path totalPath = current_path; 
-			string path = parentDirectory + NeuralNets[i].familyName + 
-			"/" + "GEN" + to_string(NeuralNets[i].generation) + 
-			"/"; 
-			string slash = "/" + path; 
-			totalPath += slash.c_str(); 
+			boost::filesystem::path totalPath = current_path;
+			string path = parentDirectory + NeuralNets[i].familyName +
+			"/" + "GEN" + to_string(NeuralNets[i].generation) +
+			"/";
+			string slash = "/" + path;
+			totalPath += slash.c_str();
 			boost::filesystem::path dir(totalPath);
 			boost::filesystem::create_directories(dir);
 
 			if(boost::filesystem::is_directory(dir)){
-				path += "NN" + i; 
-				NeuralNets[i].saveToFile(path); 	
+				path += "NN" + i;
+				NeuralNets[i].saveToFile(path);
 			}
 			else{
 				cout << "Path making failed." << endl;
 				cout << path << endl;
 			}
-			
+
 			//make loser network a child of this parent
-			NeuralNets[i - NeuralNets.size()/2] = NeuralNets[i]; 
-			NeuralNets[i - NeuralNets.size()/2].becomeOffspring(); 
+			NeuralNets[i - NeuralNets.size()/2] = NeuralNets[i];
+			NeuralNets[i - NeuralNets.size()/2].becomeOffspring();
 
 		}
 
 	}
 
-	return 0; 
+	return 0;
 }
